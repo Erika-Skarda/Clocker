8395e6e544f91eda8c94ecb609688b2ce0720242
Erika-Skarda <erikaskarda@yahoo.com.br>
2021-04-09 17:29:11 -0300

    feat: create user profile

--- clocker/components/Auth/index.js
+++ clocker/components/Auth/index.js
@@ -1,6 +1,7 @@
 import * as  React from 'react';
 import { useState, useEffect, useContext } from 'react';
 import { firebaseClient, persistenceMode } from './../../config/firebase/client';
+import axios from 'axios';
 
 const AuthContext = React.createContext([{}, () => { }]);
 
@@ -9,6 +10,8 @@ export const login = async ({ email, password }) => {
 
   try {
     await firebaseClient.auth().signInWithEmailAndPassword(email, password);
+    return  firebaseClient.auth().currentUser;
+ 
   } catch(error) {
     console.log('LOGIN ERROR',  error)
   }
@@ -19,19 +22,19 @@ export const logout = () => firebaseClient.auth().signOut();
 export const signup = async ({ email, password, username }) => {
   try {
     await firebaseClient.auth().createUserWithEmailAndPassword(email, password);
-    await login ({ email, password });
-    // setupProfile({token, username})
-    // const { data } = await axios({
-    //   method: 'post',
-    //   header: {
-    //     'Authentication': `Bearer ${user.getToken()}`
-    //   },
-    //   url: '/api/profile',
-    //   data: {
-    //     userId: user.getToken(),
-    //     username: values.username
-    //   }
-    // });
+    
+    const user = await login ({ email, password });
+    const token = await user.getTIdToken();
+
+    const { data } = await axios({
+      method: 'post',
+      url: '/api/profile',
+      data: { username },
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    //console.log(data)
   } catch(error) {
     console.log(' SIGN UP ERROR',  error)
   }
